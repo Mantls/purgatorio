b782d483ee4cdda4eeb9942ba63efff2e4a685de
Jonas Reucher <jonas@arkonatech.com>
2024-08-06 15:46:55 +0200

    rework client to handle multiple multicast-listeners

--- src/client.ts
+++ src/client.ts
@@ -11,10 +11,19 @@ export type DanteRelayPayload = {
 
 async function main() {
   const interface_ip = z.string().ip().parse(process.env["INTERFACE_ADDRESS"]);
-  const mcast = z.string().ip().parse(process.env["MCAST_ADDRESS"]);
-  const port = parseInt(process.env["PORT"]!);
   const server = new URL(z.string().url().parse(process.env["SERVER_ADDR"]));
   const ws = new WebSocket(server);
+
+  const targets = z
+    .array(
+      z
+        .string()
+        .regex(/([\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}):([\d]{1,5})/),
+    )
+    .parse(process.env["TARGETS"]?.split(",").map((s) => s.trim()))
+    .map((s) => {
+      return [s.split(":")[0], s.split(":")[1]];
+    });
 
   ws.on("open", () => {
     console.log(`Opened WebSocket to ${ws.url}`);
@@ -32,35 +41,40 @@ async function main() {
     process.exit(1);
   });
 
-  const send_client = dgram.createSocket("udp4");
-  send_client.on("listening", function() {
+  const send_client = dgram.createSocket({ type: "udp4", reuseAddr: true });
+  send_client.on("listening", function () {
     const addr = send_client.address();
     console.log(`Start listening on ${addr.address}:${addr.port}`);
     send_client.setMulticastTTL(1);
     send_client.setBroadcast(true);
     send_client.setMulticastInterface(interface_ip);
   });
-  send_client.bind(randomInt(port + 1, 2 << 16), interface_ip);
+  send_client.bind(randomInt(6000, 2 << 16), interface_ip);
 
-  const listen_client = dgram.createSocket("udp4");
-  listen_client.on("listening", function() {
-    const addr = listen_client.address();
-    console.log(`Start listening on ${addr.address}:${addr.port}`);
-    listen_client.setMulticastTTL(1);
-    listen_client.setBroadcast(true);
-    listen_client.addMembership(mcast);
-    listen_client.setMulticastInterface(interface_ip);
-  });
+  for (const [ip, port] of targets) {
+    const port_num = parseInt(port);
+    const listen_client = dgram.createSocket({ type: "udp4", reuseAddr: true });
+    listen_client.on("listening", function () {
+      const addr = listen_client.address();
+      console.log(
+        `Start listening on ${addr.address}:${addr.port} for multicast: ${ip}`,
+      );
+      listen_client.setMulticastTTL(1);
+      listen_client.setBroadcast(true);
+      listen_client.addMembership(ip);
+      listen_client.setMulticastInterface(interface_ip);
+    });
 
-  listen_client.on("message", function(message, remote) {
-    console.log(`${remote.address}:${remote.port} => ${message.toString()}`);
-    const relay_payload: DanteRelayPayload = {
-      dst_addr: mcast,
-      dst_port: port,
-      payload: message,
-    };
-    ws.send(JSON.stringify(relay_payload));
-  });
-  listen_client.bind(port);
+    listen_client.on("message", function (message, remote) {
+      console.log(`${remote.address}:${remote.port} => ${message.toString()}`);
+      const relay_payload: DanteRelayPayload = {
+        dst_addr: ip,
+        dst_port: port_num,
+        payload: message,
+      };
+      ws.send(JSON.stringify(relay_payload));
+    });
+    listen_client.bind(port_num);
+  }
 }
 main();
