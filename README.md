c95a2700bf506e0d9304345a41e323c79570a5a0
Jonas Reucher <jonas@arkonatech.com>
2024-08-06 16:42:24 +0200

    add README; change dockerfiles

---
+++ README.md
@@ -0,0 +1,31 @@
+# What is this?
+
+purgatorio is a small, lightweight service to distribute Multicast control and monitoring messages between subnets.
+
+## Usage
+
+### Server
+
+The server simply distributes messages it receivers to all clients except the one it received from.
+Simply specifiy the `PORT` environment Variable or leave it untouched to use it's default of 8080.
+
+### Client
+
+The client listens for a specified set of multicast-addresses and redistributes them to other clients via the server.
+Simultaneously it redistributes received Multicast messages from other clients that it receives from the server.
+
+_NOTE_ do not put more than one client on a subnet as this might lead to message loops.
+
+#### Usage
+
+The Client is configured via a handful of environment Variables
+
+- `INTERFACE_ADDRESS` specifies the ip-address on which it listens for multicast
+- `SERVER_ADDR` specifies the URL of the distribution-server.
+- `TARGETS` is a comma seperated list of multicast-addresses and ports for which the client listens
+
+For Example:
+
+```bash
+TARGETS="225.0.1.1:5004,225.0.1.2:5004" INTERFACE_ADDRESS="172.16.220.13" SERVER_ADDR="ws://127.0.0.1:8080" node build/client.js
+```
--- src/client.ts
+++ src/client.ts
@@ -9,72 +9,69 @@ export type DanteRelayPayload = {
   payload: Buffer;
 };
 
-async function main() {
-  const interface_ip = z.string().ip().parse(process.env["INTERFACE_ADDRESS"]);
-  const server = new URL(z.string().url().parse(process.env["SERVER_ADDR"]));
-  const ws = new WebSocket(server);
+const interface_ip = z.string().ip().parse(process.env["INTERFACE_ADDRESS"]);
+const server = new URL(z.string().url().parse(process.env["SERVER_ADDR"]));
+const ws = new WebSocket(server);
 
-  const targets = z
-    .array(
-      z
-        .string()
-        .regex(/([\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}):([\d]{1,5})/),
-    )
-    .parse(process.env["TARGETS"]?.split(",").map((s) => s.trim()))
-    .map((s) => {
-      return [s.split(":")[0], s.split(":")[1]];
-    });
-
-  ws.on("open", () => {
-    console.log(`Opened WebSocket to ${ws.url}`);
-  });
-  ws.on("message", (msg) => {
-    const parsed: DanteRelayPayload = JSON.parse(msg.toString());
-    send_client.send(JSON.stringify(parsed), parsed.dst_port, parsed.dst_addr);
-  });
-  ws.on("error", (err) => {
-    console.log(`${err.name}:${err.message}; Closing connection.`);
-    ws.close();
-  });
-  ws.on("close", () => {
-    console.log(`Lost connection to ${server.toString()}; exiting`); // we'll just crash on connection-loss and restart the process on a higher layer
-    process.exit(1);
+const targets = z
+  .array(
+    z
+      .string()
+      .regex(/([\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}):([\d]{1,5})/),
+  )
+  .parse(process.env["TARGETS"]?.split(",").map((s) => s.trim()))
+  .map((s) => {
+    return [s.split(":")[0], s.split(":")[1]];
   });
 
-  const send_client = dgram.createSocket({ type: "udp4", reuseAddr: true });
-  send_client.on("listening", function () {
-    const addr = send_client.address();
-    console.log(`Start listening on ${addr.address}:${addr.port}`);
-    send_client.setMulticastTTL(1);
-    send_client.setBroadcast(true);
-    send_client.setMulticastInterface(interface_ip);
+ws.on("open", () => {
+  console.log(`Opened WebSocket to ${ws.url}`);
+});
+ws.on("message", (msg) => {
+  const parsed: DanteRelayPayload = JSON.parse(msg.toString());
+  send_client.send(JSON.stringify(parsed), parsed.dst_port, parsed.dst_addr);
+});
+ws.on("error", (err) => {
+  console.log(`${err.name}:${err.message}; Closing connection.`);
+  ws.close();
+});
+ws.on("close", () => {
+  console.log(`Lost connection to ${server.toString()}; exiting`); // we'll just crash on connection-loss and restart the process on a higher layer
+  process.exit(1);
+});
+
+const send_client = dgram.createSocket({ type: "udp4", reuseAddr: true });
+send_client.on("listening", function () {
+  const addr = send_client.address();
+  console.log(`Start listening on ${addr.address}:${addr.port}`);
+  send_client.setMulticastTTL(1);
+  send_client.setBroadcast(true);
+  send_client.setMulticastInterface(interface_ip);
+});
+send_client.bind(randomInt(6000, 2 << 16), interface_ip);
+
+for (const [ip, port] of targets) {
+  const port_num = parseInt(port);
+  const listen_client = dgram.createSocket({ type: "udp4", reuseAddr: true });
+  listen_client.on("listening", function () {
+    const addr = listen_client.address();
+    console.log(
+      `Start listening on ${addr.address}:${addr.port} for multicast: ${ip}`,
+    );
+    listen_client.setMulticastTTL(1);
+    listen_client.setBroadcast(true);
+    listen_client.addMembership(ip);
+    listen_client.setMulticastInterface(interface_ip);
   });
-  send_client.bind(randomInt(6000, 2 << 16), interface_ip);
 
-  for (const [ip, port] of targets) {
-    const port_num = parseInt(port);
-    const listen_client = dgram.createSocket({ type: "udp4", reuseAddr: true });
-    listen_client.on("listening", function () {
-      const addr = listen_client.address();
-      console.log(
-        `Start listening on ${addr.address}:${addr.port} for multicast: ${ip}`,
-      );
-      listen_client.setMulticastTTL(1);
-      listen_client.setBroadcast(true);
-      listen_client.addMembership(ip);
-      listen_client.setMulticastInterface(interface_ip);
-    });
-
-    listen_client.on("message", function (message, remote) {
-      console.log(`${remote.address}:${remote.port} => ${message.toString()}`);
-      const relay_payload: DanteRelayPayload = {
-        dst_addr: ip,
-        dst_port: port_num,
-        payload: message,
-      };
-      ws.send(JSON.stringify(relay_payload));
-    });
-    listen_client.bind(port_num);
-  }
+  listen_client.on("message", function (message, remote) {
+    console.log(`${remote.address}:${remote.port} => ${message.toString()}`);
+    const relay_payload: DanteRelayPayload = {
+      dst_addr: ip,
+      dst_port: port_num,
+      payload: message,
+    };
+    ws.send(JSON.stringify(relay_payload));
+  });
+  listen_client.bind(port_num, interface_ip);
 }
-main();
