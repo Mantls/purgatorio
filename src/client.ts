28fc748b3ccdca1cc97346cbae1e7de5d7b1d255
Jonas Reucher <jonas@arkonatech.com>
2025-04-15 18:55:45 +0200

    send raw buffer

--- src/client.ts
+++ src/client.ts
@@ -29,7 +29,7 @@ ws.on("open", () => {
 });
 ws.on("message", (msg) => {
   const parsed: DanteRelayPayload = JSON.parse(msg.toString());
-  send_client.send(JSON.stringify(parsed), parsed.dst_port, parsed.dst_addr);
+  send_client.send(parsed.payload, parsed.dst_port, parsed.dst_addr);
 });
 ws.on("error", (err) => {
   console.log(`${err.name}:${err.message}; Closing connection.`);
